1f2b57352b167e164646474aafb987f2e8fb5fe7
Vignesh <vigneshragu501@gmail.com>
2020-12-17 13:45:40 +0530

    Added sub method

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,18 @@ class Calculator
     {
       return num1+num2;
     }
+    
+    public Static int sub(int num1,int num2)
+    {
+      return num1-num2;
+    }
+    
+    
+    
     public static void main(String []args)
     {
     System.out.println(add(10,20));
+    System.out.println(sub(30,20));
     }
     
 }
