de973c959f63d078abd147379e8f702a65e25bf1
iamvigneshR <vigneshragu501@gmail.com>
2020-12-18 13:43:46 +0530

    Product method created in Calc class from nb branch.

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -11,6 +11,11 @@ public class Calc {
 		System.out.println("Difference = " +(num1-num2));
 	}
 	
+	public void product(int num1,int num2)
+	{
+		System.out.println("Product = " +(num1*num2));
+	}
+	
 	
 
 }
