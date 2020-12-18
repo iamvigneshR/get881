6a337c18c66f5a595d52ab6e1762659b4fcf97cf
Vignesh <vigneshragu501@gmail.com>
2020-12-18 13:05:52 +0530

    Update Calc.java
    Changes in subtraction method...

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -1,14 +1,14 @@
 package com.lti.model;
 
 public class Calc {
-	public void add(int num1,int num2)
+	public void addition(int num1,int num2)
 	{
 		System.out.println("Sum = " +(num1+num2));
 	}
 	
-	public void sub(int num1,int num2)
+	public void subtraction(int num1,int num2)
 	{
-		System.out.println("Sum = " +(num1-num2));
+		System.out.println("Difference = " +(num1-num2));
 	}
 	
 	
